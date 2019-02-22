c9aa46575f406462196de3c6e8beaf6e5d39d44a
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2019-02-21 17:38:09 -0800

    set up support controller

---
+++ backend/src/main/java/com/comp680/backend/controllers/SupportController.java
@@ -0,0 +1,30 @@
+package com.comp680.backend.controllers;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RestController
+@RequestMapping("support")
+@CrossOrigin(origins="http://localhost:4200", allowedHeaders="*")
+public class SupportController { 
+	@GetMapping("/")
+	public ResponseEntity<?> example() {
+		return new ResponseEntity<>("hello", HttpStatus.OK);
+	}
+}

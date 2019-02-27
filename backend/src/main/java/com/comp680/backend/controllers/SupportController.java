a323813abf245dd08e0ba3f90e39709d1bbe5436
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2019-02-26 21:28:50 -0800

    send email

--- backend/src/main/java/com/comp680/backend/controllers/SupportController.java
+++ backend/src/main/java/com/comp680/backend/controllers/SupportController.java
@@ -1,30 +1,35 @@
 package com.comp680.backend.controllers;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
-@RequestMapping("support")
-@CrossOrigin(origins="http://localhost:4200", allowedHeaders="*")
-public class SupportController { 
-	@GetMapping("/")
-	public ResponseEntity<?> example() {
-		return new ResponseEntity<>("hello", HttpStatus.OK);
+public class SupportController {
+	@Autowired
+    private JavaMailSender sender;
+
+	@RequestMapping(value = "/support")
+	public String sendEmail() {
+        MimeMessage message = sender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        try {
+            helper.setTo("demo@gmail.com");
+            helper.setText("Greetings :)");
+            helper.setSubject("Mail From Spring Boot");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return "Error while sending mail ..";
+        }
+        sender.send(message);
+        return "Mail Sent Success!";
 	}
 }
+

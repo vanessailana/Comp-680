0fe2e779646a477c38bd5d9fc5cd45cede8fd474
vanessa <vanessa@vanessas-mbp.lan>
2019-03-02 07:51:13 -0800

    added chat bot url generted from diagflow

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { CallbackComponent } from './callback/callback.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ChatbotComponent } from './chatbot/chatbot.component';
 
 
 export const ROUTES: Routes = [
-     { path: 'profile', component: ProfileComponent },
+  { path: 'profile', component: ProfileComponent },
+    { path: 'support', component: ChatbotComponent },
   { path: 'callback', component: CallbackComponent },
   { path: '**', redirectTo: '' }
 ];

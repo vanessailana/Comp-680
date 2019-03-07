1d0ca52ec8013e78657665d1faedf239a0bcce32
vanessa <vanessa@vanessas-MacBook-Pro.local>
2019-03-06 20:27:10 -0800

    test auth service

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,35 +1,27 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async,inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+import { AuthService } from './auth/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
-describe('AppComponent', () => {
+describe('AuthService', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,HttpClientModule,AppModule
       ],
       declarations: [
-        AppComponent
+       
       ],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+ it('should be created 2', inject([AuthService], (service: AuthService) => {
+    expect(service).toBeTruthy();
+  }));
 
-  it(`should have as title 'frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('frontend');
-  });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to frontend!');
-  });
+
 });

f50315e1a737367eb8c85423ac4d8a7bca866b32
marvinharcsun <marvin.harootoonyan.570@my.csun.edu>
2019-03-10 01:48:25 -0800

    Cors Fix For Profile Please validate

---
+++ backend/src/main/java/com/comp680/backend/models/UsersRepository.java
@@ -0,0 +1,11 @@
+package com.comp680.backend.models;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersRepository extends CrudRepository<Users, Long> {
+
+    Users findByEmail(String email);
+}
+
